98435636bda2eab479893becce166c8722e98a46
MWesselkamp <marieke.wesselkamp@posteo.de>
2024-10-07 18:51:35 +0000

    updated forecast module

--- ecland-emulator/forecast_module.py
+++ ecland-emulator/forecast_module.py
@@ -14,6 +14,8 @@ import time
 import torch
 
 from torch import tensor
+from models import *
+from data_module import *
 
 if torch.cuda.is_available():
     DEVICE = "cuda:0"
@@ -29,24 +31,61 @@ class ForecastModule:
     Initialize class with Pytorch lighning or DLMC xgb model type.
     """
     
-    def __init__(self, model, my_device = None):
+    def __init__(self, hpars ,config, my_device = None):
         
-        if 'MLP' in type(model).__name__:
-            print("Instance is mlp model")
-            self.filename = 'mlp'
-        else:
-            print("Model type is:", type(model))
-            print("Don't know model type")
-        
-        self.model = model
+        self.hpars = hpars
+        self.config = config
 
         if my_device is None:
             self.my_device = DEVICE
         else:
             self.my_device = my_device
         
-    
-    def get_test_data(self, dataset, chunk_idx = (0, None)):
+    def initialise_dataset(self):
+
+        datamodule = NonLinRegDataModule(self.config)
+        dataset = EcDataset(self.config,
+                    self.config["test_start"],
+                    self.config["test_end"])
+
+        self.input_clim_dim = len(dataset.static_feat_lst)
+        self.input_met_dim = len(dataset.dynamic_feat_lst)
+        self.input_state_dim = len(dataset.targ_lst)
+        self.output_dim = len(dataset.targ_lst)  # Number of output targets
+        self.output_diag_dim = 0 # len(dataset.targ_diag_lst)
+
+        return dataset
+
+    def load_model(self):
+
+        self.model = MLPregressor(input_clim_dim = self.input_clim_dim,
+                             input_met_dim = self.input_met_dim,
+                             input_state_dim = self.input_state_dim,
+                             hidden_dim = self.hpars['hidden_dim'],
+                             output_dim = self.output_dim,
+                             output_diag_dim = self.output_diag_dim,
+                             batch_size=self.config["batch_size"],
+                             learning_rate=self.hpars['learning_rate'], 
+                             lookback = None, 
+                             rollout=self.config["roll_out"], 
+                             dropout=self.hpars['dropout'], 
+                             weight_decay = self.hpars['weight_decay'], 
+                             loss = self.hpars['loss'],
+                             device = 'cpu', 
+                             targets = self.config["targets_prog"],
+                             db_path = self.config["file_path"])
+
+        path_to_checkpoint = self.config["model_path"]
+        use_checkpoint = os.listdir(path_to_checkpoint)[-1]
+        path_to_best_checkpoint = os.path.join(path_to_checkpoint, use_checkpoint)  # trainer.checkpoint_callback.best_model_path
+        print("Load model from checkpoint: ", path_to_best_checkpoint)
+        checkpoint = torch.load(path_to_best_checkpoint, map_location=torch.device('cpu'))
+        torch.set_float32_matmul_precision("high")
+        self.model.load_state_dict(checkpoint['state_dict'])
+
+        return self.model
+
+    def load_test_data(self, dataset):
 
         """
         Args:
@@ -55,20 +94,18 @@ class ForecastModule:
             
         Needs to be called before step forecast! 
         """
-        dataset.x_idxs = chunk_idx
         
         X_static, X_met, Y_prog = dataset.load_data() 
-            
         self.dataset = dataset
 
         return X_static, X_met, Y_prog
 
-    def get_climatology(self, file_path, targ_lst):    
+    def get_climatology(self):    
 
-        climatology = xr.open_dataset(file_path)
-        climatology_mu = climatology['clim_6hr_mu'].sel(variable=targ_lst).values
+        climatology = xr.open_dataset(self.config['climatology_path'])
+        climatology_mu = climatology['clim_6hr_mu'].sel(variable=self.config['targets_prog']).values
         climatology_mu = np.tile(climatology_mu, (2, 1, 1))
-        climatology_std = climatology['clim_6hr_std'].sel(variable=targ_lst).values
+        climatology_std = climatology['clim_6hr_std'].sel(variable=self.config['targets_prog']).values
         climatology_std = np.tile(climatology_std, (2, 1, 1))
         return climatology_mu, climatology_std
         
@@ -95,7 +132,7 @@ class ForecastModule:
         
         start_time = time.time()
         
-        if self.filename == 'mlp':
+        if self.config['model'] == 'mlp':
             
             with torch.no_grad():
                 
@@ -117,7 +154,7 @@ class ForecastModule:
         print(Y_prog_prediction.shape)
         
         self.dynamic_features = Y_prog
-        self.dynamic_features_prediction = tensor(Y_prog_prediction) if self.filename == 'xgb' else Y_prog_prediction
+        self.dynamic_features_prediction = tensor(Y_prog_prediction) if self.config['model'] == 'xgb' else Y_prog_prediction
                 
         print("Backtransforming")
         # make class object for succeeding access.
@@ -142,15 +179,15 @@ class ForecastModule:
                                           "location": self.dataset.ds_ecland['x'],
                                           "variable": self.dataset.dynamic_feat_lst},
                                      dims=["time", "x", "variable"])
-        obs_array.to_netcdf(os.path.join(save_to, f'fc_ecland_{self.filename}.nc'))
-        print("saved reference data to: ", os.path.join(save_to, f'fc_ecland_{self.filename}.nc'))
+        obs_array.to_netcdf(os.path.join(save_to, f'fc_ecland_{self.config['model']}.nc'))
+        print("saved reference data to: ", os.path.join(save_to, f'fc_ecland_{self.config['model']}.nc'))
 
         preds_array = xr.DataArray(self.dynamic_features_prediction,
                                   coords={"time": self.dataset.full_times[:self.dynamic_features_prediction.shape[0]],
                                           "location": self.dataset.ds_ecland['x'],
                                           "variable": self.dataset.dynamic_feat_lst},
                                       dims=["time", "x", "variable"])
-        preds_array.to_netcdf(os.path.join(save_to, f'fc_ailand_{self.filename}.nc'))
-        print("saved forecast to: ", os.path.join(save_to, f'fc_ecland_{self.filename}.nc'))
+        preds_array.to_netcdf(os.path.join(save_to, f'fc_ailand_{self.config['model']}.nc'))
+        print("saved forecast to: ", os.path.join(save_to, f'fc_ecland_{self.config['model']}.nc'))
 
         return self.dynamic_features, self.dynamic_features_prediction
