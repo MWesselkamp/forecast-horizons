511e253d80a21f6652a07361768c147628792787
MWesselkamp <marieke.wesselkamp@gmail.com>
2025-02-11 17:39:31 +0100

    evluate with crps

--- s_ricker/stochastic_ricker.py
+++ s_ricker/stochastic_ricker.py
@@ -1,6 +1,18 @@
+"""
+Steady state predictability analysis with stochastic ricker model.
+author: @mariekewesselkamp
+"""
+import sys
+print(sys.path)
+
 import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+from scipy.stats import norm
+from scores.probability import crps_cdf
+from joblib import Parallel, delayed, parallel_backend
 
 np.random.seed(42)
 
@@ -77,91 +89,197 @@ def observations(r, k, N_init, sigma_N, error_size, tsteps):
     }, coords={"time": np.arange(tsteps)})
 
 
-def plot_observations(dat_train):
+def plot_observations(dat_train, plot=True):
     plt.plot(dat_train.coords['time'].values, dat_train["dyn_true"], color = "black", label = "True dynamic")
     plt.plot(dat_train.coords['time'].values, dat_train["dyn_proc"], color ="blue", label = "Observed dynamic")
     plt.ylabel("Relative size")
     plt.xlabel("Time")
     plt.legend()
-    plt.show()
+    if plot:
+        plt.show()
 
-horiz = 30
+def plot_setup(plot=True):
+    fig, ax = plt.subplots(1, 1)
+    ax.fill_between(np.arange(horiz),climatological_mean+2*climatological_std, climatological_mean-2*climatological_std, color = "lightgray")
+    ax.plot(np.arange(horiz), output.squeeze(), color="blue", linewidth=0.8, alpha =0.2)
+    ax.plot(np.full((horiz, 1), climatological_mean), color="red", linewidth=0.9, alpha =0.8, label = 'climatological mean')
+    ax.plot(np.arange(horiz), y_obs[:horiz], color="black", linewidth=0.9, alpha =0.9, label = 'observations')
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Relative size")
+    plt.legend()
+    if plot:
+        plt.show()
+
+def plot_mae(plot=True):
+
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(ensemble_error.transpose(), color= "lightgray")
+    ax.plot(ensemble_mean_error, color= "black", label = "Ensemble average")
+    ax.plot(climatological_error.transpose(), color= "red", label = "Climatological average")
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Mean absolute error")
+    plt.legend()
+    if plot:
+        plt.show()
+
+# helper function
+def compute_cdf(values):
+    mu, sigma = norm.fit(values) # fit normal distribution to fc
+    values_cdf = norm.cdf(values, loc = mu, scale = sigma)
+    return values_cdf
+
+def run_forecast(N_init, ensemble_size, time_horizon):
+
+    params = sample_pars(ns=ensemble_size,
+                        r=r,
+                        k=k,
+                        sigma_N=sigma_N,
+                        error_size=parameter_error)
+
+    # initialise forecast with IC uncertainty
+    X = np.random.normal(loc=N_init, scale=IC_error, size = ensemble_size) 
+
+    output = np.zeros((time_horizon, ensemble_size, 1))
+    output[0,:,:] = X[:, np.newaxis]
+
+    for t in range(1, time_horizon):
+
+        params = sample_pars(ns=ensemble_size,
+                        r=r,
+                        k=k,
+                        sigma_N=sigma_N,
+                        error_size=parameter_error)
+        X = output[t-1, :, :]
+        sim = ricker_sim(X.squeeze(), params)
+        output[t, :, :] = sim.values[:, np.newaxis]
+
+    return output
+
+def crps_over_time(time_horizon, forecast, observation):
+
+    fc_crps = np.zeros(time_horizon)
+    fc_sorted = np.sort(forecast, axis=1)
+
+    for t in range(time_horizon):
+
+        fc = fc_sorted[t,:,:].squeeze() # forecast at t
+        fc_cdf  = compute_cdf(fc)
+        observed = observation[t].values
+
+        fc_array = xr.DataArray(coords={'rel_size': fc}, data=fc_cdf)
+        obs_array = xr.DataArray(observed)
+
+        fc_crps[t] = crps_cdf(fc_array, obs_array, threshold_dim='rel_size').total.values.round(6)
+
+    return(fc_crps)
+
+horiz = 50 # forecast horizon for forecast model
+horiz_obs = 100 # forecast horizon for creating observational truth
+clim_horiz = 1000 # forecast horizon during climatological forecast
+
+ne = 500 # Ensemble size for climatology (use same size for forecast).
 
 # True initial conditions
-r = 0.05
-k = 1
-sigma_N = 0.001 # observation error
+r = 0.05 # growth rate
+k = 1 # carrying capacity
+sigma_N = 0.001 # observation error for creating observational truth 
 N_init = k # set initial conditions to carrying capacity for steady state dynamics
 
-parameter_error = 0.04 # precision for parameter samples
-IC_error = 0.0001 # initial conditions error
+parameter_error = 0.04 # relative precision for scale of parameter distribution
+IC_error = 0.0001 # assumed initial conditions error
 
+# simulate observations with observation error and stochastic parameters
 dat_train = observations(r = r, k=k, N_init = N_init, sigma_N = sigma_N, 
-                          error_size = parameter_error, tsteps = horiz) # simulate observations with observation error and stochastic parameters
+                          error_size = parameter_error, tsteps = clim_horiz) 
 # plot_observations(dat_train)
+y_obs = dat_train['dyn_proc'][-horiz_obs:]
 
-N_init = dat_train['dyn_proc'][0] # initial conditions for forecast
-print("Initial conditions", N_init.values)
+N_init = y_obs[0] # initial conditions for climatological forecast
+print("Initial conditions for climatological forecast: ", N_init.values)
 
-# Create climatology with long-term simulation and error propagation 
+# Create climatology with long-term simulation and error propagation in r and k and IC. 
+climatology = run_forecast(N_init=y_obs[0], ensemble_size=500, time_horizon=clim_horiz)
 
-ne = 500 # Ensemble size. production run should be 200 - 5000, depending on what your computer can handle
-clim_horiz = 1000 #days, forecast horizon during forecast
+print("Climatological mean: ", climatology.squeeze().mean())
+print("Climatological SD: ", climatology.squeeze().std())
 
-params = sample_pars(ns=ne,
-                     r=r,
-                     k=k,
-                     sigma_N=sigma_N,
-                     error_size=parameter_error)
+climatological_mean = climatology.squeeze().mean()
+climatological_std = climatology.squeeze().std()
 
-X = np.random.normal(loc=N_init, scale=IC_error, size = ne)
-#plt.hist(X)
-#plt.show()
+# use saturated climatological distribution for comparison with forecast distribution
+climatology_short = climatology[-horiz:, :, :] 
 
-climatology = np.zeros((clim_horiz, ne, 1))
-climatology[0,:,:] = X[:, np.newaxis]
+# initial observation as initial conditions for forecast
+# Run forecast from n = 0 over horiz with 500 members with error propagation in r and k and IC. 
+output = run_forecast(N_init=y_obs[0], ensemble_size=500, time_horizon=horiz)
 
-for t in range(1, clim_horiz):
+# MAE 
+ensemble_error = (output.squeeze().transpose()-y_obs[:horiz].values) # Absolute error
+ensemble_mean_error = ensemble_error.mean(axis=0) # Mean absolute error
+climatological_error = (np.full((horiz), climatological_mean) - y_obs[:horiz].values) # absolute mean error
 
-    params = sample_pars(ns=ne,
-                     r=r,
-                     k=k,
-                     sigma_N=sigma_N,
-                     error_size=parameter_error)
-    X = climatology[t-1, :, :]
-    sim = ricker_sim(X.squeeze(), params)
-    climatology[t, :, :] = sim.values[:, np.newaxis]
+plot_setup(plot=True)
+plot_mae(plot=True)
 
-# Run forecast from n = 0 over horiz
+# CRPS 
+# Compute the crps for all time steps
 
-ne = 500 # Ensemble size. production run should be 200 - 5000, depending on what your computer can handle
+crps_fc = crps_over_time(horiz, output, y_obs)
+crps_clim = crps_over_time(horiz, climatology_short, y_obs)
+crpss = (1 - crps_fc/crps_clim)
 
-params = sample_pars(ns=ne,
-                     r=r,
-                     k=k,
-                     sigma_N=sigma_N,
-                     error_size=parameter_error)
+fig, ax = plt.subplots(1, 1)
+ax.plot(crpss)
+ax.set_ylabel("CRPSS")
+ax.set_xlabel("Time")
+plt.show()
 
-X = np.random.normal(loc=N_init, scale=IC_error, size = ne)
-#plt.hist(X)
-#plt.show()
+# Now do this over multiple time steps with different initalisation of N_init from y_obs
 
-output = np.zeros((horiz, ne, 1))
-output[0,:,:] = X[:, np.newaxis]
 
-for t in range(1, horiz):
+def compute_crpss(i):
+    """Function to run forecast, compute CRPS, and return CRPSS for one iteration."""
 
-    params = sample_pars(ns=ne,
-                     r=r,
-                     k=k,
-                     sigma_N=sigma_N,
-                     error_size=parameter_error)
-    X = output[t-1, :, :]
-    sim = ricker_sim(X.squeeze(), params)
-    output[t, :, :] = sim.values[:, np.newaxis]
+    output = run_forecast(N_init=y_obs[i], ensemble_size=500, time_horizon=horiz_obs)
+    climatology = run_forecast(N_init=y_obs[i], ensemble_size=500, time_horizon=clim_horiz)
+    climatology_short = climatology[-horiz_obs:, :, :] 
 
-plt.plot(climatology[-horiz:, :, :].squeeze(), color="lightgray", linewidth=0.8)
-plt.plot(output.squeeze(), color="blue", linewidth=0.8)
-plt.xlabel("Time")
-plt.ylabel("Relative size")
+    y_obs_short = y_obs[i:]
+
+    crps_fc = crps_over_time(len(y_obs_short), output, y_obs_short)
+    crps_clim = crps_over_time(len(y_obs_short), climatology_short, y_obs_short)
+    return (1 - crps_fc/crps_clim)
+
+# Run in parallel (uses all but one available CPU cores)
+crpss_results = Parallel(n_jobs=-1)(delayed(compute_crpss)(i) for i in range(horiz))
+# reset worker usage to 1.
+with parallel_backend("loky"):
+    pass
+
+# Store results in preallocated array
+shifted_matrix = np.full((horiz, 2 * horiz), np.nan)
+for i in range(horiz):
+    shifted_matrix[i, i:] = crpss_results[i]  # Shifted placement
+
+print("CRPSS shifted matrix shape:", shifted_matrix.shape)
+print("CRPSS shifted_matrix rows are varying initial forecast times")
+print("CRPSS shifted_matrix columns are lead times")
+
+
+fig, ax = plt.subplots(1, 1)
+sns.heatmap(shifted_matrix, cmap="viridis", annot=False, linewidths=0.5)
+plt.show()
+
+print(np.nanmean(shifted_matrix, axis=0).shape)
+predicted_lead_times = shifted_matrix[:,horiz:]
+
+fig, ax = plt.subplots(1, 1)
+#ax.fill_between(np.arange(horiz), 
+#    np.mean(predicted_lead_times, axis=0)+np.std(predicted_lead_times, axis=0), 
+#    np.mean(predicted_lead_times, axis=0)-np.std(predicted_lead_times, axis=0), color = "lightgray")
+ax.plot(np.mean(predicted_lead_times, axis=0))
+plt.show()
+
+fig, ax = plt.subplots(1, 1)
+ax.boxplot(predicted_lead_times, vert=True, patch_artist=True)
 plt.show()
